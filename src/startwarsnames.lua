43a1a7b7b6b22fe2a6e2183cc4a9294f9fcb791a
Perkovec <perkovec@icloud.com>
2015-09-06 21:04:40 +0300

    Update startwarsnames.lua

--- src/startwarsnames.lua
+++ src/startwarsnames.lua
@@ -1,10 +1,18 @@
 local SWN = {}
-local names = require("names.lua")
+local names = require("names")
+local past = ""
 
 SWN.all = names
 
-SWN.random = function(){
-  return names[math.random( #names )]
-}
+SWN.random = function ()
+    math.randomseed( os.time() )
+    local name = names[math.random( 1, #names )]
+    if (name == past) then
+        SWN.random()
+    else
+        past = name
+        return name
+    end
+end
 
 return SWN
